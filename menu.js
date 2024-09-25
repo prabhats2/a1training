e3e0adc933d788bc5c352d0669acaef4853b1395
prabhats2 <prabhatnlh@gmail.com>
2024-09-25 17:17:21 +0530

    tawk.io modify

--- menu.js
+++ menu.js
@@ -71,7 +71,24 @@ document.write(`  <!-- Sub Header -->
         </div>
     </div>
 </header>
-<!-- ***** Header Area End ***** -->`)
+<!-- ***** Header Area End ***** -->
+
+
+
+
+`)
+
+//tawk io script 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66bb21d20cca4f8a7a754fad/1i55fo8ds';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 //if (window.location.pathname.endsWith('.html')) {
   //  const newPath = window.location.pathname.replace('.html', '');
   //  window.history.replaceState(null, '', newPath);
