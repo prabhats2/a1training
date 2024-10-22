adffcf52345de696bb263167a0bc7b0aef9cdd58
prabhats2 <prabhatnlh@gmail.com>
2024-10-22 16:11:40 +0530

    test4

---
+++ interview-Q-Ans.js
@@ -0,0 +1,30 @@
+ let javascript =[
+"javascript.pdf",'j1.pdf','j3.pdf','j4.pdf'   
+]
+let kubernetes =[
+    "k1.pdf",'k2.pdf','k3.pdf'
+    ] 
+
+    let courses = [
+        {
+          title: "JavaScript",
+          btn_heading: "JavaScript",
+          pdf: ["javascript.pdf", "j1.pdf", "j3.pdf", "j4.pdf"]
+        },
+        {
+          title: "Kubernetes",
+          btn_heading: "Kubernetes Interview Questions",
+          pdf: ["kubernetes.pdf", "k11.pdf", "k2.pdf", "k3.pdf"]
+        },
+        {
+          title: "Shell Scripting",
+          btn_heading: "Shell Scripting Interview Questions",
+          pdf: ["Shell_scripting.pdf", "s1.pdf", "s2.pdf", "s3.pdf"]
+        },
+        {
+          title: "MS Office",
+          btn_heading: "MS Office Interview Questions",
+          pdf: ["MS_Office.pdf", "ms1.pdf", "ms2.pdf", "ms3.pdf"]
+        }
+      ];
+      
